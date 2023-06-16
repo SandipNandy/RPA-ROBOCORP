1c19b7d35ae02ee7c80b8cd0e71c33c2294ec145
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-06-16 09:46:59 +0530

    Update RPA_Desktop.py

--- Desktop_Application/RPA_Desktop.py
+++ Desktop_Application/RPA_Desktop.py
@@ -1,7 +1,5 @@
 from RPA.Desktop import Desktop
 desktop = Desktop()
-
-
 def navigate_all_positive_stocks():
     stock_elems = desktop.find_elements("alias:GreenStock")
     for stock_elem in stock_elems:
